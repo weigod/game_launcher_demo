a7e1285aa7b3887c59e80d8e7273aa8cf473aa50
weigod <78773111+weigod@users.noreply.github.com>
2023-09-04 10:35:41 +0800

    Revert "完成demo"

--- streamer/app.js
+++ streamer/app.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import LaunchLoadView from '../launch/LaunchLoadView';
-import './app.hycss';
+import { UI } from '@hyext/hy-ui'
+import React, { Component } from 'react'
+import AppConfig from '../config'
+import { controlPanelInVisible, controlPanelVisible } from './utils/controlPanelVisible'
+import Launch from '../launch/Launch'
+import LaunchLoadView from '../launch/LaunchLoadView'
+import { LogInfo } from '../launch/util/Log';
+import { offGameMessage } from '../launch/stream'
+import './app.hycss'
 
-const TAG = 'app';
+const TAG = "app"
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+    this.state = {
+      initFinish: false
+    }
+    this.param = null;
+    this.init();
+  }
+
+  async init() {
+    await Launch.init({
+      gameBusizName:AppConfig.game_name,
+      gameConfigApi:this.gameConfigApi,
+      exit:this.disposal,
+      finish:()=>{
+        // controlPanelInVisible()
+      }
+    });
+
+    Launch.start();
+    LogInfo("AppConfig", JSON.stringify(AppConfig))
+    this.state.initFinish = true;
+    this.setState({ ...this.state })
+  }
+
+
+  async gameConfigApi() {
+    let rsp = {};
+    rsp.gameUrl = AppConfig.game_url;
+    rsp.gameMd5 = AppConfig.game_md5;
+    rsp.gameExeName = AppConfig.game_exe_name;
+    return rsp;
+  }
+
+  async disposal() {
+    offGameMessage();
+    hyExt.panel.disposal()
   }
 
   render() {
     return (
-      <LaunchLoadView></LaunchLoadView>
-    );
+      <>
+        {this.state.initFinish && <LaunchLoadView>hello world</LaunchLoadView>}
+      </>
+    )
   }
 }
 
-export default App;
+export default App
